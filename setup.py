f30608d77e3ffb9f1e865d5f77f886c689f8e87a
rohitdavas <rohitchoudhary.git@gmail.com>
2020-12-18 02:36:26 +0530

    requirements added.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='LibraryManagementSystem',
+    version='1',
+    packages=[''],
+    url='',
+    license='None',
+    author='davas',
+    author_email='rohitchoudhary.git@gmail.com',
+    description='Setup file'
+)
